9ea7db66b96f7875587169e7e8125e2114e9adff
Wenress <marco.lettiero@gmail.com>
2021-05-03 15:41:57 +0200

    Android app requirements added with microphone settings

---
+++ Assets/Scripts/AndroidPermissionRequirements.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Android;
+
+public class AndroidPermissionRequirements : MonoBehaviour
+{
+    void Awake()
+    {
+#if PLATFORM_ANDROID
+        if (!Permission.HasUserAuthorizedPermission(Permission.Microphone))
+        {
+            Permission.RequestUserPermission(Permission.Microphone);
+            //dialog = new GameObject();
+        }
+#endif
+    }
+}
