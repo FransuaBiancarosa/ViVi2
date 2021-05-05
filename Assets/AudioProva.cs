fbd3d7ea974075cf3554dbb230d852c92c807235
Wenress <marco.lettiero@gmail.com>
2021-05-05 16:56:57 +0200

    First setup of dissonance-salsa-mirror

---
+++ Assets/AudioProva.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioProva : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.P))
+            GetComponent<AudioSource>().Play();
+    }
+}
